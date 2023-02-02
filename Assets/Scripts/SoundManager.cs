589b3307f0ba8f8f07ab6c07f7245124c3297247
Guilherme Manassés <guilhermemanasses98@gmail.com>
2023-02-02 11:10:26 -0300

    Added SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    //--- Core Functions ---
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+}
