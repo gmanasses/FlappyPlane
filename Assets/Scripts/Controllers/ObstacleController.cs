25cc873da60e7dd3bf1fcd821cf9a70ccea914b0
Guilherme Manassés <guilhermemanasses98@gmail.com>
2023-02-06 17:25:18 -0300

    ordering layers

--- Assets/Scripts/Controllers/ObstacleController.cs
+++ Assets/Scripts/Controllers/ObstacleController.cs
@@ -4,9 +4,14 @@ public class ObstacleController : MonoBehaviour {
 
     // --- Private Declarations ---
     [SerializeField] private int _speed = 5;
+    [SerializeField] private float _yPositionVariation;
 
 
     // --- Core Functions ---
+    private void Awake() {
+        this.transform.Translate(Vector3.up * Random.Range(-_yPositionVariation, _yPositionVariation));
+    }
+
     private void Update() {
         this.transform.Translate(Vector3.left * this._speed * Time.deltaTime);
     }
