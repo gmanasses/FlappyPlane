d0281b4b474f829ee3b57a3c1e25f0bd8a91e8e0
Guilherme Manassés <guilhermemanasses98@gmail.com>
2023-02-07 10:14:51 -0300

    add game over

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour {
 
+    // --- Private Declarations ---
+    [SerializeField] private Image _gameOverImage;
+
+
     // --- Functions ---
     public void GameOver() {
+        //freeze game
         Time.timeScale = 0;
+
+        //show game over image
+        this._gameOverImage.gameObject.SetActive(true);
     }
     
 }
