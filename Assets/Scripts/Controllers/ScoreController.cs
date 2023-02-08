dc1f25af1e15227da1389467933b151fe325d49e
Guilherme Manassés <guilhermemanasses98@gmail.com>
2023-02-08 11:59:30 -0300

    Add Audio

--- Assets/Scripts/Controllers/ScoreController.cs
+++ Assets/Scripts/Controllers/ScoreController.cs
@@ -5,17 +5,20 @@ public class ScoreController : MonoBehaviour {
 
     // --- Private Declarations ---
     [SerializeField] private Text _scoreText;
+    private AudioSource _scoreSound;
     private int _score;
 
 
     // --- Core Functions ---
     private void Awake() {
+        _scoreSound = this.GetComponent<AudioSource>();
         _score = 0;
     }
 
 
     // --- Functions ---
     public void AddScore() {
+        _scoreSound.Play();
         _score++;
         UpdateUI();
     }
