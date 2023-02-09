9a69879bc01cadedd81d2246afa56bb66d1f836e
Guilherme Manassés <guilhermemanasses98@gmail.com>
2023-02-09 18:35:07 -0300

    Added Difficulty Manager

--- Assets/Scripts/Generators/ObstacleGenerator.cs
+++ Assets/Scripts/Generators/ObstacleGenerator.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 public class ObstacleGenerator : MonoBehaviour {
 
     // --- Private Declarations ---
-    [SerializeField] private float _generationTime;
+    [SerializeField] private float _generationTimeEasy;
+    [SerializeField] private float _generationTimeHard;
     [SerializeField] private GameObject _obstaclePrefab;
+    private DifficultyManager _difficultyManager;
     private float _chronometer;
 
 
     // --- Core Functions ---
     private void Awake() {
-        _chronometer = _generationTime;
+        _chronometer = _generationTimeEasy;
+    }
+
+    private void Start() {
+        _difficultyManager = GameObject.FindObjectOfType<DifficultyManager>();
     }
 
     private void Update() {        
         _chronometer -= Time.deltaTime;
         if(_chronometer < 0) {
             GameObject.Instantiate(_obstaclePrefab, this.transform.position, Quaternion.identity);
-            _chronometer = _generationTime;
+            _chronometer = Mathf.Lerp(_generationTimeEasy, _generationTimeHard, _difficultyManager.Difficulty);
         }
     }
 
---
+++ Assets/Scripts/Managers/DifficultyManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour {
+
+    // --- Public Declarations ---
+    public float Difficulty { get; private set; }
+
+    // --- Private Declarations ---
+    [SerializeField] private float _timeToMaxDifficulty;
+    private float _elapsedTime;
+
+
+    // --- Core Functions ---
+    private void Update() {
+        _elapsedTime += Time.deltaTime;
+
+        Difficulty = _elapsedTime / _timeToMaxDifficulty;
+        Difficulty = Mathf.Min(1, Difficulty);
+    }
+
+}
