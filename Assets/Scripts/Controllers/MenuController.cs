56a2f4919b29ce81af64994192da782546e21769
Guilherme Manassés <guilhermemanasses98@gmail.com>
2023-02-13 16:01:03 -0300

    Created Menu Scene, Coop Scene

---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    // --- Private Declarations ---
+    [SerializeField] Button _singlePlayerButton, _coopButton, _settingsButton;
+
+}
