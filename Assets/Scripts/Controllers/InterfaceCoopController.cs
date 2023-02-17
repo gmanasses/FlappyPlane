3eb644b4ba692e8b2905794232564cbc98182e22
Guilherme Manassés <guilhermemanasses98@gmail.com>
2023-02-17 14:52:48 -0300

    Game finalization

--- Assets/Scripts/Controllers/InterfaceCoopController.cs
+++ Assets/Scripts/Controllers/InterfaceCoopController.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class InterfaceCoopController : MonoBehaviour {
 
     // --- Private Declarations ---
-    [SerializeField] private GameObject _line, _background;
+    [SerializeField] private GameObject _line, _background, _pauseButton;
 
 
     // --- Functions ---
     public void ShowAndHideScoreBackground(bool show) {
         _background.SetActive(show);
         _line.SetActive(show);
+    }
+
+    public void ShowPauseButton() {
+        _pauseButton.SetActive(true);
     }
 
 }
